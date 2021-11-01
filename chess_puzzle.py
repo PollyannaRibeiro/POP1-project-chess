68ebf5271a1b5b866be4f48b3bce1fc1570a1acb
Polly <23667108+PollyannaRibeiro@users.noreply.github.com>
2021-11-01 12:22:23 +0000

    added class Piece

--- chess_puzzle.py
+++ chess_puzzle.py
@@ -18,7 +18,9 @@ class Piece:
     side : bool #True for White and False for Black
     def __init__(self, pos_X : int, pos_Y : int, side_ : bool):
         '''sets initial values'''
-
+        self.pos_x = pos_X
+        self.pos_y = pos_Y
+        self.side = side_
 
 Board = tuple[int, list[Piece]]
 
